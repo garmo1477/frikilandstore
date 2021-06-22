e16021e7ce4633378108bb5c25c2411ec4386016
garmo <59025832+garmo1477@users.noreply.github.com>
2021-06-22 13:34:02 +0200

    cambios

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -2,14 +2,26 @@
     <div class="row">
         <div class="col-md-12 banner">
             <div class="row">
-                <div class="col-md-4 centered">
-                    <h1 class="text-center">{{ __('¡Crea ya tu cuenta y empieza a comprar!') }}</h1></br>
-                    <div class="d-flex justify-content-center">
-                        <a href="{{ route('registro') }}" class="btn btn-primary">{{ __('Registrarse') }}</a>
-                    </div>                    
+                <div class="d-none d-md-block col-md-4 centered">
+                    @guest
+                        <h1 class="text-center">{{ __('¡Crea ya tu cuenta y empieza a comprar!') }}</h1></br>
+                        <div class="d-flex justify-content-center">
+                            <a href="{{ route('registro') }}" class="btn btn-primary">{{ __('Registrarse') }}</a>
+                        </div>
+                    @endguest
+                    @auth
+                        <div class="text-center centered mt-5">
+                            <h2 class="">
+                                {{ __('¡Bienvenido/a :user a Frikiland Store!', ['user' => auth()->user()->name]) }}
+                            </h2>
+                            <p class="">{{ __('Accede a tu perfil para realizar cualquier gestión') }}</p>
+                        </div>
+
+                    @endauth
+
                 </div>
             </div>
-            
+
         </div>
     </div>
 </section>
--- resources/views/partials/content/seller_form.blade.php
+++ resources/views/partials/content/seller_form.blade.php
@@ -1,51 +1,61 @@
 <style>
-    .register-form div.spacing{
+    .register-form div.spacing {
         padding: 6%;
     }
 </style>
-<div class="row register-form mt-4">    
+<div class="row register-form mt-4">
     <div class="col-lg-6 spacing">
-        <div class="">
-            <div class="section-title text-center pt-4 pb-4">
-                
-                <h2 class="text-white">{{ __('¿Quieres vender todos tus productos en Frikiland Store') }}</h2>
-                <p class="text-white">{{ __('Regístrate como vendedor y podrás publicarlos') }}</p>
-            </div>
+        <div>
             @if (!session('error-login'))
                 @include('partials.errorsForm')
             @endif
-            <!-- signup form -->
-            <form id="seller-form" class="signup-form"
-                action="{{ route('register') }}" method="POST">
-                @csrf
-                <div class="row">
-                    <div class="form-group">
-                        <input class="form-control" type="hidden" name="role" value="{{ \App\User::SELLER }}" />
-                    </div>
-                    <div class="form-group col-md-6">
-                        <input class="form-control" autocomplete="off" name="name" value="{{ old('name') }}"
-                            type="text" placeholder="{{ __('Nombre') }}" />
-                    </div>
-                    <div class="form-group col-md-6">
-                        <input class="form-control" autocomplete="off" name="email" value="{{ old('email') }}"
-                            type="text" placeholder="{{ __('Correo Electrónico') }}" />
-                    </div>
+            @guest
+                <div class="section-title text-center pt-4 pb-4">
+
+                    <h2 class="text-white">{{ __('¿Quieres vender todos tus productos en Frikiland Store') }}</h2>
+                    <p class="text-white">{{ __('Regístrate como vendedor y podrás publicarlos') }}</p>
                 </div>
-                <div class="row">
-                    <div class="form-group col-md-6">
-                        <input class="form-control" name="password" type="password" />
+                <!-- signup form -->
+                <form id="seller-form" class="signup-form" action="{{ route('register') }}" method="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="form-group">
+                            <input class="form-control" type="hidden" name="role" value="{{ \App\User::SELLER }}" />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input class="form-control" autocomplete="off" name="name" value="{{ old('name') }}"
+                                type="text" placeholder="{{ __('Nombre') }}" />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input class="form-control" autocomplete="off" name="email" value="{{ old('email') }}"
+                                type="text" placeholder="{{ __('Correo Electrónico') }}" />
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <input class="form-control"  name="password_confirmation" type="password" />
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="password" type="password" />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input class="form-control" name="password_confirmation" type="password" />
+                        </div>
                     </div>
+                    <div class="row justify-content-end">
+                        <button class="btn btn-primary mr-3">{{ __('Crear cuenta de vendedor') }}</button>
+                    </div>
+                </form>
+            @endguest
+            @auth
+                <h2 class="text-white">{{ __('Todos los productos de Frikiland Store', ['user' => auth()->user()->name]) }}</h2>
+                <p class="text-white">{{ __('Visita nuestro apartados de productos y encuentra lo que buscas') }}</p>
+                <div class="d-flex justify-content-center">
+                    <a href="{{ route('videogames.index') }}" class="btn btn-primary">{{ __('Ver Juegos') }}</a>
+                    <a href="" class="btn btn-primary ml-4">{{ __('Ver Merchan') }}</a>
                 </div>
-                <div class="row justify-content-end">
-                    <button class="btn btn-primary mr-3">{{ __('Crear cuenta de vendedor') }}</button>
-                </div>                
-            </form>            
+            @endauth
+
             <div id="success-seller-signup" class="section-title text-white text-left">
 
-            </div>          
+            </div>
 
         </div>
     </div>
